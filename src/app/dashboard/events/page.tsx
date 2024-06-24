c633b83bda7077bbf34d958f94e210036cd456cf
alex <alex.sanders@ukr.net>
2024-06-24 17:49:55 +0300

    Create NewEventForm

--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -1,5 +1,16 @@
+import NewEventForm from '../components/NewEventForm/NewEventForm'
+import styles from './page.module.scss'
+
 export default function EventsPage() {
 	return (
-		<div>Events</div>
+		<div className={styles.page}>
+			<h3 className={styles.title}>Event</h3>
+
+			<h3 className={styles.title}>Add new event</h3>
+
+			<div className={styles.addNewEvent}>
+				<NewEventForm />
+			</div>
+		</div>
 	)
 }
