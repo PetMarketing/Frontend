857c55db6d3da33f6476c4081f52c49dba24d829
Ваня <7328652@gmail.com>
2024-06-25 12:19:08 +0300

    change  form on Team page to «formik»

--- src/components/TeamForm/TeamForm.tsx
+++ src/components/TeamForm/TeamForm.tsx
@@ -12,109 +12,43 @@ import clip from '@/assets/clip.png'
 import welcome from '@/assets/welcome.png'
 
 import styles from './TeamForm.module.scss'
+import { useFormik } from 'formik'
+import { validationSchema } from '@/utils/yupTeamForm'
 
 const apiKey = process.env.NEXT_PUBLIC_API_KEY
 
 const TeamForm = () => {
-	const [formData, setFormData] = useState<ITeamFormData>({
-		name: '',
-		position: '',
-		email: '',
-		description: '',
-		imagePath: null,
-	})
-
-	const [validationErrors, setValidationErrors] = useState<Partial<ITeamFormDataErrors>>(
-		{},
-	)
-	const [showErrorMessage, setShowErrorMessage] = useState<boolean>(false)
 	const [showSentConfirm, setShowSentConfirm] = useState<boolean>(false)
 	const [loading, setLoading] = useState<boolean>(false)
 
-	const handleChange = (
-		e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLInputElement>,
-	) => {
-		const { name, value } = e.target
-		setFormData(prevState => ({
-			...prevState,
-			[name]: value,
-		}))
-		setValidationErrors(prevState => ({
-			...prevState,
-			[name]: '',
-		}))
-	}
-
-	const handleCVChange = (e: ChangeEvent<HTMLInputElement>) => {
-		const file = e.target.files?.[0]
-		setFormData(prevState => ({
-			...prevState,
-			imagePath: file || null,
-		}))
-	}
-
-	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-
-		const errors: Partial<ITeamFormDataErrors> = {}
-		if (!formData.name.trim()) {
-			errors.name = 'Enter Name'
-		}
-		if (!formData.position.trim()) {
-			errors.position = 'Enter Position'
-		}
-		if (!formData.email.trim()) {
-			errors.email = 'Enter e-mail'
-		} else if (!validateEmail(formData.email)) {
-			errors.email = 'Invalid email address'
-		}
-		if (!formData.description.trim()) {
-			errors.description = 'Enter description'
-		}
-		if (formData.imagePath === null) {
-			errors.imagePath = 'No file'
-		}
-
-		setValidationErrors(errors)
-
-		if (Object.keys(errors).length === 0) {
-			setLoading(true)
+	const formik = useFormik<ITeamFormData>({
+		initialValues: {
+			name: '',
+			position: '',
+			email: '',
+			description: '',
+			imagePath: null,
+		},
+		validationSchema,
+		onSubmit: async values => {
 			try {
-				const formDataToSend = new FormData()
-				formDataToSend.append('name', formData.name)
-				formDataToSend.append('email', formData.email)
-				formDataToSend.append('position', formData.position)
-				formDataToSend.append('description', formData.description)
-				if (formData.imagePath) {
-					formDataToSend.append('imagePath', formData.imagePath)
-				}
-
-				const response = await fetch(`${apiKey}/team-form`, {
+				setLoading(true)
+				console.log(values)
+				await fetch(`${apiKey}/team-form`, {
 					method: 'POST',
-					body: formDataToSend,
+					body: JSON.stringify(values),
 				})
 
-				if (response.ok) {
-					setFormData({
-						name: '',
-						position: '',
-						email: '',
-						description: '',
-						imagePath: null,
-					})
-					setLoading(false)
-					setShowSentConfirm(true)
-				}
+				setLoading(false)
 			} catch (error) {
-				setShowErrorMessage(true)
+				console.log('error:', error)
+				setLoading(false)
+				setShowSentConfirm(true)
+			} finally {
+				formik.resetForm()
 			}
-		}
-	}
-
-	const validateEmail = (email: string): boolean => {
-		const regex = /\S+@\S+\.\S+/
-		return regex.test(email)
-	}
+		},
+	})
 
 	return (
 		<div className={styles.teamForm}>
@@ -132,136 +66,153 @@ const TeamForm = () => {
 							<Loader className={styles.loader} />
 						</div>
 					) : (
-						<form className={styles.form} onSubmit={handleSubmit}>
-							{showErrorMessage && (
-								<p style={{ color: 'red' }}>
-									Sorry, something went wrong:( <br />
-									Check if you filled out the forms correctly
-								</p>
-							)}
-							<div className={styles.inputContainer} style={{ gridArea: 'name' }}>
-								<input
-									type='text'
-									id='name'
-									name='name'
-									placeholder=''
-									value={formData.name}
-									onChange={handleChange}
-									style={
-										validationErrors.name ? { borderColor: 'red' } : undefined
-									}
-								/>
-								{validationErrors.name ? (
-									<label htmlFor='name' style={{ color: 'red' }}>
-										{validationErrors.name}
-									</label>
-								) : (
-									<label htmlFor='name'>Name</label>
-								)}
-							</div>
-							<div
-								className={styles.inputContainer}
-								style={{ gridArea: 'position' }}
-							>
-								<input
-									type='text'
-									id='position'
-									name='position'
-									placeholder=''
-									value={formData.position}
-									onChange={handleChange}
-									style={
-										validationErrors.position
-											? { borderColor: 'red' }
-											: undefined
-									}
-								/>
-								{validationErrors.position ? (
-									<label htmlFor='name' style={{ color: 'red' }}>
-										{validationErrors.position}
-									</label>
-								) : (
-									<label htmlFor='name'>Position</label>
-								)}
-							</div>
-							<div className={styles.inputContainer} style={{ gridArea: 'email' }}>
-								<input
-									type='email'
-									id='email'
-									name='email'
-									placeholder=''
-									value={formData.email}
-									onChange={handleChange}
-									style={
-										validationErrors.email ? { borderColor: 'red' } : undefined
-									}
-								/>
-								{validationErrors.email ? (
-									<label htmlFor='name' style={{ color: 'red' }}>
-										{validationErrors.email}
-									</label>
-								) : (
-									<label htmlFor='name'>Email address</label>
-								)}
-							</div>
-							<div
-								className={styles.inputContainer}
-								style={{ gridArea: 'description' }}
-							>
-								<input
-									id='description'
-									name='description'
-									placeholder=''
-									value={formData.description}
-									onChange={handleChange}
-									style={
-										validationErrors.description
-											? { borderColor: 'red' }
-											: undefined
-									}
-								/>
-								{validationErrors.description ? (
-									<label htmlFor='name' style={{ color: 'red' }}>
-										{validationErrors.description}
-									</label>
-								) : (
-									<label htmlFor='name'>Short description</label>
-								)}
-							</div>
-							{formData.imagePath === null ? (
-								<label
-									htmlFor='cv'
-									className={styles.fileUpload}
-									style={
-										validationErrors.imagePath
-											? { border: '1px solid red', color: 'red' }
-											: undefined
-									}
+						<>
+							<form className={styles.form} onSubmit={formik.handleSubmit}>
+								<div
+									className={styles.inputContainer}
+									style={{ gridArea: 'name' }}
 								>
-									Attach your CV +
-								</label>
-							) : (
-								<label htmlFor='cv' className={styles.fileUpload}>
-									Attached{' '}
-									<Image
-										src={clip}
-										alt='clip icon'
-										style={{ marginLeft: '5px' }}
+									<input
+										type='text'
+										id='name'
+										name='name'
+										placeholder=''
+										value={formik.values.name}
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+										style={
+											formik.touched.name && formik.errors.name
+												? { borderColor: 'red' }
+												: undefined
+										}
 									/>
-								</label>
-							)}
-							<input
-								type='file'
-								id='cv'
-								name='cv'
-								accept='.pdf'
-								onChange={handleCVChange}
-								style={{ display: 'none' }}
-							/>
-							<Button variant='primary' type='submit' className={styles.sendBtn}>
-								Send
-							</Button>
-						</form>
+									{formik.touched.name && formik.errors.name ? (
+										<label htmlFor='name' style={{ color: 'red' }}>
+											{formik.errors.name}
+										</label>
+									) : (
+										<label htmlFor='name'>Name</label>
+									)}
+								</div>
+								<div
+									className={styles.inputContainer}
+									style={{ gridArea: 'position' }}
+								>
+									<input
+										type='text'
+										id='position'
+										name='position'
+										placeholder=''
+										value={formik.values.position}
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+										style={
+											formik.touched.position && formik.errors.position
+												? { borderColor: 'red' }
+												: undefined
+										}
+									/>
+									{formik.touched.position && formik.errors.position ? (
+										<label htmlFor='position' style={{ color: 'red' }}>
+											{formik.errors.position}
+										</label>
+									) : (
+										<label htmlFor='position'>Position</label>
+									)}
+								</div>
+								<div
+									className={styles.inputContainer}
+									style={{ gridArea: 'email' }}
+								>
+									<input
+										type='email'
+										id='email'
+										name='email'
+										placeholder=''
+										value={formik.values.email}
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+										style={
+											formik.touched.email && formik.errors.email
+												? { borderColor: 'red' }
+												: undefined
+										}
+									/>
+									{formik.touched.email && formik.errors.email ? (
+										<label htmlFor='email' style={{ color: 'red' }}>
+											{formik.errors.email}
+										</label>
+									) : (
+										<label htmlFor='email'>Email address</label>
+									)}
+								</div>
+								<div
+									className={styles.inputContainer}
+									style={{ gridArea: 'description' }}
+								>
+									<input
+										id='description'
+										name='description'
+										placeholder=''
+										value={formik.values.description}
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+										style={
+											formik.touched.description && formik.errors.description
+												? { borderColor: 'red' }
+												: undefined
+										}
+									/>
+									{formik.touched.description && formik.errors.description ? (
+										<label htmlFor='description' style={{ color: 'red' }}>
+											{formik.errors.description}
+										</label>
+									) : (
+										<label htmlFor='description'>Short description</label>
+									)}
+								</div>
+
+								{formik.values.imagePath === null ? (
+									<label
+										htmlFor='cv'
+										className={styles.fileUpload}
+										style={
+											formik.touched.imagePath && formik.errors.imagePath
+												? { border: '1px solid red', color: 'red' }
+												: undefined
+										}
+									>
+										Attach your CV +
+									</label>
+								) : (
+									<label htmlFor='cv' className={styles.fileUpload}>
+										Attached{' '}
+										<Image
+											src={clip}
+											alt='clip icon'
+											style={{ marginLeft: '5px' }}
+										/>
+									</label>
+								)}
+								<input
+									type='file'
+									id='cv'
+									name='imagePath'
+									accept='.pdf'
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+									style={{ display: 'none' }}
+								/>
+
+								<Button
+									variant='primary'
+									type='submit'
+									className={styles.sendBtn}
+								>
+									Send
+								</Button>
+							</form>
+						</>
 					)}
 				</>
 			)}
