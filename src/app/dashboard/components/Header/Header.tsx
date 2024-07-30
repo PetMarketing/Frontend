ec573b65886809fb87c58b0eba0181710e727801
alex <alex.sanders@ukr.net>
2024-07-30 11:15:16 +0300

    fix: update dashboard header, show info about user

--- src/app/dashboard/components/Header/Header.tsx
+++ src/app/dashboard/components/Header/Header.tsx
@@ -1,20 +1,23 @@
 import Image from 'next/image';
 
+import { getSession } from '@/services/auth/auth.service';
+
 import styles from './Header.module.scss';
 
 export default async function Header() {
-	const session = undefined;
+	const session = await getSession();
+
+	const avatarSrc = session.user.imagePath ? session.user.imagePath : '/default-avatar.svg';
 
 	return (
 		<header className={styles.header}>
 			<div className={styles.userInfo}>
 				<div className={styles.imageWrapper}>
-					<Image src='/default-avatar.svg' width={80} height={80} alt='avatar' className={styles.image} />
+					<Image src={avatarSrc} width={80} height={80} alt='avatar' className={styles.image} />
 				</div>
-
 				<div className={styles.infoWrapper}>
-					<div className={styles.name}>{session?.user?.name}</div>
-					<div className={styles.role}>{session?.user?.role}</div>
+					<div className={styles.name}>{session.user.name}</div>
+					<div className={styles.role}>{session.user.role}</div>
 				</div>
 			</div>
 		</header>
