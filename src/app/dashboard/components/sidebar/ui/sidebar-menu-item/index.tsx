3366a25f1d0b64326e9a8dd190c8408aa2ca7d32
alex <alex.sanders@ukr.net>
2024-08-04 19:14:22 +0300

    Dashboard: Add sidebar auto-hide on menu link click

--- src/app/dashboard/components/sidebar/ui/sidebar-menu-item/index.tsx
+++ src/app/dashboard/components/sidebar/ui/sidebar-menu-item/index.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 
+import { useSidebar } from '@/store/sidebar.store'
+
 import styles from './styles.module.scss'
 
 interface ISidebarMenuItemProps {
@@ -10,8 +12,10 @@ interface ISidebarMenuItemProps {
 }
 
 export default function SidebarMenuItem({ href, Icon, name, active }: ISidebarMenuItemProps) {
+	const toggleSidebar = useSidebar((state) => state.toggleSidebar)
+
 	return (
-		<Link href={href} className={`${styles.link} ${active ? styles.active : ''}`}>
+		<Link href={href} className={`${styles.link} ${active ? styles.active : ''}`} onClick={toggleSidebar}>
 			<div className={styles.icon}>
 				<Icon />
 			</div>
