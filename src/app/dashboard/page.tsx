484f5fa12a5c4af4c493f1aae19aceb5ffa408a7
alex <alex.sanders@ukr.net>
2024-08-05 21:02:30 +0300

    include credentials to function for test refresh token

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,6 +8,24 @@ export default function Dashboard() {
 
 		const res = await fetch(`${baseURL}/admin/refresh-token`, {
 			method: 'POST',
+			credentials: 'include',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+		});
+
+		const data = await res.json();
+
+		console.log('data: ', data);
+	};
+
+	const testSession = async () => {
+		const baseURL = process.env.NEXT_PUBLIC_API_KEY;
+
+		const res = await fetch('https://hop-hop-shop-9h26.onrender.com/api/cart/add/5/', {
+			method: 'POST',
+			credentials: 'include',
 			headers: {
 				'Accept': 'application/json',
 				'Content-Type': 'application/json',
@@ -25,6 +43,7 @@ export default function Dashboard() {
 
 			<div className={styles.test}>
 				<button onClick={handleRefreshToken}>Refresh Token</button>
+				<button onClick={testSession}>Test cookie</button>
 			</div>
 		</div>
 	)
