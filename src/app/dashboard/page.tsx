7e7d57952e54eba02d054f37e7084b4ba346426f
alex <alex.sanders@ukr.net>
2024-08-05 20:14:17 +0300

    Add function for test refresh token

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,31 @@
-export default async function Dashboard() {
+'use client'
+
+import styles from './page.module.scss'
+
+export default function Dashboard() {
+	const handleRefreshToken = async () => {
+		const baseURL = process.env.NEXT_PUBLIC_API_KEY;
+
+		const res = await fetch(`${baseURL}/admin/refresh-token`, {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+		});
+
+		const data = await res.json();
+
+		console.log('data: ', data);
+	};
+
 	return (
 		<div>
 			<h3>Dashboard</h3>
+
+			<div className={styles.test}>
+				<button onClick={handleRefreshToken}>Refresh Token</button>
+			</div>
 		</div>
 	)
 }
