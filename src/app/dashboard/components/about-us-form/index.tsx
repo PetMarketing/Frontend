2e72e2855fe8d107b2faed52026a4c9b32f732c8
alex <alex.sanders@ukr.net>
2024-09-11 10:23:30 +0300

    Update create about-us form

--- src/app/dashboard/components/about-us-form/index.tsx
+++ src/app/dashboard/components/about-us-form/index.tsx
@@ -1,70 +1,94 @@
 'use client'
 
-import { useState } from 'react';
-import { Form, Formik, FormikHelpers } from 'formik'
+import { useState } from 'react'
+import { Form, Formik } from 'formik'
 import * as Yup from 'yup'
 
-import Loader from '@/components/Loader/Loader';
-import ErrorMessage from '@/components/ErrorMessage/ErrorMessage';
-import FormikInput from '../FormikInput/FormikInput';
-import Button from '../Button/Button';
-
-import { ISession } from '@/types/ISession';
+import Loader from '@/components/Loader/Loader'
+import ErrorMessage from '@/components/ErrorMessage/ErrorMessage'
+import FormikInput from '../FormikInput/FormikInput'
+import Button from '../Button/Button'
 
 import styles from './styles.module.scss'
 
-
 export default function AboutUsForm() {
-    const [isError, setIsError] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isSuccess, setIsSuccess] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
+	const [isError, setIsError] = useState(false)
+	const [isLoading, setIsLoading] = useState(false)
+	const [isSuccess, setIsSuccess] = useState(false)
+	const [errorMessage, setErrorMessage] = useState('')
 
-    const initialValues = {
-        title: '',
-        description: '',
-        imageAlt : '',
-        imagePath: '',
-    }
+	const initialValues = {
+		title: '',
+		description: '',
+		imageAlt: '',
+		imagePath: '',
+	}
 
-    const validationSchema = Yup.object().shape({
-        title: Yup.string().required('Required'),
-        description: Yup.string().required('Required'),
-        imageAlt: Yup.string().required('Required'),
-        imagePath: Yup.mixed().required('Image is required')
-    });
+	const validationSchema = Yup.object().shape({
+		title: Yup.string().required('Required'),
+		description: Yup.string().required('Required'),
+		imageAlt: Yup.string().required('Required'),
+		imagePath: Yup.mixed().required('Image is required'),
+	})
 
-    const submitHandler = async (values: any, actions: any) => {
-        setIsError(false); // Resetting the previous error before a new request
-        setIsLoading(true); // Indicate that the request has been started
-        setErrorMessage(''); // Reset the previous error message
+	const submitHandler = async (values: any, actions: any) => {
+		setIsError(false) // Resetting the previous error before a new request
+		setIsLoading(true) // Indicate that the request has been started
+		setErrorMessage('') // Reset the previous error message
 
-        console.log('values')
-    }
+		console.log('values')
+	}
 
-    return (
-        isLoading ? (
-            <div className={styles.loaderWrapper} >
-                <Loader className={styles.loader} />
-            </div>
-        ) : (
-            <>
-                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={(values, actions) => submitHandler(values, actions)}>
-                    <Form className={styles.form}>
-                        <FormikInput className={styles.input} type='text' name='title' id='title' label='Title' />
+	return isLoading ? (
+		<div className={styles.loaderWrapper}>
+			<Loader className={styles.loader} />
+		</div>
+	) : (
+		<>
+			<Formik
+				initialValues={initialValues}
+				validationSchema={validationSchema}
+				onSubmit={(values, actions) => submitHandler(values, actions)}
+			>
+				<Form className={styles.form}>
+					<FormikInput
+						className={styles.input}
+						type='text'
+						name='title'
+						id='title'
+						label='Title'
+					/>
 
-                        <FormikInput className={styles.input} type='text' name='description' id='description' label='Description' />
+					<FormikInput
+						className={styles.input}
+						type='text'
+						name='description'
+						id='description'
+						label='Description'
+					/>
 
-                        <FormikInput className={styles.input} type='file' name='image' id='image' label='Image' accept='image/jpeg,image/jpg' />
+					<FormikInput
+						className={styles.input}
+						type='file'
+						name='image'
+						id='image'
+						label='Image'
+						accept='image/jpeg,image/jpg'
+					/>
 
-                        <FormikInput className={styles.input} type='text' name='imageAlt' id='imageAlt' label='Image description' />
+					<FormikInput
+						className={styles.input}
+						type='text'
+						name='imageAlt'
+						id='imageAlt'
+						label='Image description'
+					/>
 
-                        <Button className={styles.button} text='Add new block' type='submit' />
-                    </Form>
-                </Formik>
+					<Button className={styles.button} text='Add new block' type='submit' />
+				</Form>
+			</Formik>
 
-                {isError && <ErrorMessage err={errorMessage} />}
-            </>
-        )
-    )
+			{isError && <ErrorMessage err={errorMessage} />}
+		</>
+	)
 }
